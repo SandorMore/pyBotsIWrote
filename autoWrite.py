26adf99627eed1a2523464986ef93bfeccc83359
SandorMore <msaso0725@gmail.com>
2024-11-22 20:29:08 +0100

    fuck cait

--- autoWrite.py
+++ autoWrite.py
@@ -1,7 +1,29 @@
 import keyboard
+import pyautogui
+import pygetwindow as gw
+import time
+import threading
+import win32gui
+import win32con
+import sys
 
-keyboard.send("enter")
+def enter():
+    keyboard.send("enter")
+def bring_window_to_foreground(window_title):
+    """
+    Brings the specified window to the foreground if it exists.
+    
+    :param window_title: The title of the window to bring to the foreground
+    """
+    try:
+        hwnd = win32gui.FindWindow(None, window_title)
+        if hwnd:
+            win32gui.ShowWindow(hwnd, win32con.SW_RESTORE)
 
-keyboard.write("Skibidi")
+            win32gui.SetForegroundWindow(hwnd)
 
-keyboard.send("enter")
+    except Exception as e:
+        print(f"Error bringing window to foreground: {e}")
+enter()
+keyboard.write("Hi i'm a bot made by the player using me! He is probably AFK right now peeing or some shit so dont even bother swapping he will be back in a minute! I will ban cait for him in the meanwhile! FUCK CAITLYN FUCK CAITLYN FUCK CAITLYN FUCK CAITLYN FUCK CAITLYN FUCK CAITLYN FUCK CAITLYN FUCK CAITLYN FUCK CAITLYN")
+enter()
